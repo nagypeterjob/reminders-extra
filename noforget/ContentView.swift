c194b1816b961defa8d5bb6d670496b0e37a0007
nagypeterjob <peter.nagy@bitrise.io>
2025-05-07 22:57:51 +0200

    Initial Commit

---
+++ noforget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  noforget
+//
+//  Created by Nagy Peter on 2025. 05. 07..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ noforget/noforgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  noforgetApp.swift
+//  noforget
+//
+//  Created by Nagy Peter on 2025. 05. 07..
+//
+
+import SwiftUI
+
+@main
+struct noforgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
